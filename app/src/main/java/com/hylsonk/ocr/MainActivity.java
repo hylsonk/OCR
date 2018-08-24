e7805ca0569a262c484e9c71fdbbf3ef6bc1defb
Hylson Kobayashi <hylsonk@gmail.com>
2018-08-23 22:56:09 -0300

    Projeto de OCR com Firebase

---
+++ app/src/main/java/com/hylsonk/ocr/MainActivity.java
@@ -0,0 +1,106 @@
+package com.hylsonk.ocr;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button snapBtn;
+    private Button detectBtn;
+    private ImageView imageView;
+    private TextView textView;
+
+    private Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        snapBtn = findViewById(R.id.snapBtn);
+        detectBtn = findViewById(R.id.detectBtn);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.txtView);
+
+        snapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        detectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detectText();
+            }
+        });
+    }
+
+    //    Abre a camera para tirar foto
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            bitmap= (Bitmap) extras.get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    private void detectText() {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionTextDetector detector = FirebaseVision.getInstance().getVisionTextDetector();
+        detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                processTxt(firebaseVisionText);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+    private void processTxt(FirebaseVisionText firebaseVisionText) {
+        List<FirebaseVisionText.Block> blocks = firebaseVisionText.getBlocks();
+        if (blocks.size() == 0){
+            Toast.makeText(MainActivity.this, "No text :(",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        for (FirebaseVisionText.Block block : firebaseVisionText.getBlocks()){
+            String text = block.getText();
+            textView.setTextSize(24);
+            textView.setText(text);
+        }
+    }
+
+}
